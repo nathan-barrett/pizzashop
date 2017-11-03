8b5e62221b5519ea453ca5ee070339ba969d3f63
Nathan Barrett <demeules.barrett@gmail.com>
2017-11-03 10:18:06 -0700

    add checkbox, beggining of back end logic in js

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+//back - end logic
+function Pizza() {
+  this.size
+  this.toppings = [];
+}
+
+Pizza.prototype.Price = function() {
+  return //figure this out//
+}
+
+
+
+
+
+
+//front-end logic//
+$(function){
+  $("form#pizza-time").submit(function(event){
+    event.preventDefault();
+    var pizzaPrice = 10;
+
+  });
+});
