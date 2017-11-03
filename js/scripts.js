aaa369c5b49fb414dd36bd26b3724041633e19fe
Nathan Barrett <demeules.barrett@gmail.com>
2017-11-03 11:51:41 -0700

    hot damn. figured it out

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,10 @@ function Pizza(size, veggies, meat, special) {
   this.meat = meat;
   this.special = special;
 }
+var newPizza = new Pizza;
 
 Pizza.prototype.Price = function() {
-  return //figure this out//
+  return this.size + this.veggies.length + (this.meat.length * 2) + (this.special.length * 3);
 }
 
 function resetForm() {
@@ -15,20 +16,30 @@ function resetForm() {
 }
 
 
-
-
-
-
-
 //front-end logic//
-$(function){
+$(function() {
   $("form#pizza-time").submit(function(event){
     event.preventDefault();
     var pizzaSize = parseInt($("select#pizza-size").val());
-    console.log(pizzaSize)
     var veggieTopping = $("input:checkbox[name=topping]:checked").map(function(){
       return $(this).val();
-    })console.log(veggieTopping)
+    });
+    var meatTopping = $("input:checkbox[name=meatTopping]:checked").map(function(){
+      return $(this).val();
+    });
+    var specialTopping = $("input:checkbox[name=specialTopping]:checked").map(function(){
+      return $(this).val();
+    });
+    var userPizza = new Pizza (pizzaSize, veggieTopping, meatTopping, specialTopping);
+
+    console.log(userPizza.size);
+    console.log(specialTopping.length);
+    console.log(meatTopping.length);
+    console.log(veggieTopping.length);
+    debugger
+    outputPrice = userPizza.Price();
+
+    console.log(outputPrice);
 
   });
 });
